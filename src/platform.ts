e50ae1ed26f2c6fb97a975a9ef16f5154f5db72b
dash16 <dash16@gmail.com>
2026-06-02 16:05:31 -0700

    Update platform.ts
    remove debug writeback test

--- src/platform.ts
+++ src/platform.ts
@@ -8,7 +8,6 @@ import { PLATFORM_NAME, PLUGIN_NAME } from './settings.js';
 interface MonsterPlatformConfig extends PlatformConfig {
 	email?: string;
 	password?: string;
-	rgbicWritebackTest?: boolean;
 	pollIntervalSeconds?: number;
 }
 
@@ -90,28 +89,6 @@ export class MonsterSmartLighting implements DynamicPlatformPlugin {
 				}
 
 				this.discoveredCacheUUIDs.push(uuid);
-				
-				if (this.config.rgbicWritebackTest) {
-					this.log.warn('Running temporary RGBIC writeback test for %s. This will overwrite pic04.', device.productName);
-				
-					await this.monsterApi.setRgbicPreset(device.dsn, 4, 'Homebridge Test', [
-						{ type: 'rgb', red: 255, green: 0, blue: 0 },
-						{ type: 'rgb', red: 255, green: 0, blue: 0 },
-						{ type: 'rgb', red: 255, green: 0, blue: 0 },
-				
-						{ type: 'rgb', red: 0, green: 255, blue: 0 },
-						{ type: 'rgb', red: 0, green: 255, blue: 0 },
-						{ type: 'rgb', red: 0, green: 255, blue: 0 },
-				
-						{ type: 'rgb', red: 0, green: 0, blue: 255 },
-						{ type: 'rgb', red: 0, green: 0, blue: 255 },
-						{ type: 'rgb', red: 0, green: 0, blue: 255 },
-				
-						{ type: 'white', value: 100 },
-						{ type: 'white', value: 100 },
-						{ type: 'white', value: 100 },
-					], 100);
-				}
 			}
 
 			for (const [uuid, accessory] of this.accessories) {
